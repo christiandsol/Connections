e68047b4de70442b9c3b266fc36d3c908fbd18bd
Christian Solano <christiansolano@Christians-MacBook-Air-2.local>
2024-02-22 22:13:07 -0800

    Tranfer state-control to App level - remove random functionality - remove re-rendering on submit error

--- src/App.js
+++ src/App.js
@@ -1,146 +1,75 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import WordTile from "./components/WordTile";
 import "./App.css";
 const YELLOW = "1";
 const GREEN = "2";
 const BLUE = "3";
 const PURPLE = "4";
+const HEIGHT = 4;
+const WIDTH = 4;
 
-const choices = [];
 export default function App() {
     const [hasWon, setHasWon] = useState(false);
     const [hasYellow, setHasYellow] = useState(false);
     const [hasGreen, setHasGreen] = useState(false);
     const [hasBlue, setHasBlue] = useState(false);
     const [hasPurple, setHasPurple] = useState(false);
-
-    const words = {
-        "Hello": YELLOW,
-        "Rizz": GREEN,
-        "asdfa": BLUE,
-        "CRIZZY": PURPLE,
-        "JOSEPh": YELLOW,
-        "KIM": GREEN,
-        "CHRISTIAN": BLUE,
-        "WOW": PURPLE,
-        "EGGERT": YELLOW,
-        "STAHL": GREEN,
-        "STREANd": BLUE,
-        "fasdf": PURPLE,
-        "yello": YELLOW,
-        "green": GREEN,
-        "Blue": BLUE,
-        "purple": PURPLE,
-    }
-    const inDisplay = {}
-
-    const getRandomEntry = (obj) => {
-        const keys = Object.keys(obj);
-
-        const randomKey = keys[Math.floor(Math.random() * keys.length)];
-        if (randomKey in inDisplay) {
-            return getRandomEntry(obj);
-        }
-        inDisplay[randomKey] = true;
-        console.log(randomKey);
-        return randomKey;
-    }
-
-    const handleClick = (word) => {
+    const [choices, setChoices] = useState([]);
+    const words = [
+        { word: "Hello", color: YELLOW },
+        { word: "Rizz", color: GREEN },
+        { word: "asdfa", color: BLUE },
+        { word: "CRIZZY", color: PURPLE },
+        { word: "JOSEPh", color: YELLOW },
+        { word: "KIM", color: GREEN },
+        { word: "CHRISTIAN", color: BLUE },
+        { word: "WOW", color: PURPLE },
+        { word: "EGGERT", color: YELLOW },
+        { word: "STAHL", color: GREEN },
+        { word: "STREANd", color: BLUE },
+        { word: "fasdf", color: PURPLE },
+        { word: "yello", color: YELLOW },
+        { word: "green", color: GREEN },
+        { word: "Blue", color: BLUE },
+        { word: "purple", color: PURPLE },
+    ]
+    const handleClick = (index) => {
         if (choices.length >= 4) {
             return;
         }
-        if (choices.includes(word)) {
-            choices.splice(choices.indexOf(word), 1);
+        if (choices.includes(index)) {
+            choices.splice(choices.indexOf(index), 1);
             return;
         }
-        choices.push(word);
-        console.log(choices);
+        setChoices([...choices, index]);
     }
     const checkCorrect = (choices) => {
-        console.log(choices);
-        if (choices.length != 4) {
-            return;
-        }
-        if (words[choices[0]] === words[choices[1]] && words[choices[1]] === words[choices[2]]
-            && words[choices[2]] === words[choices[3]]) {
-            if (words[choices[0]] === YELLOW) {
-                setHasYellow(true);
-            } if (words[choices[0]] === GREEN) {
-                setHasGreen(true);
-            } if (words[choices[0]] === BLUE) {
-                setHasBlue(true);
-            } if (words[choices[0]] === PURPLE) {
-                setHasPurple(true);
-            }
-            return;
-        }
+        console.log(choices); 
     }
+
     return (
         <>
             <div className="game">
                 <div className="words-box">
-                    <div className="container">
-                        <WordTile onClick={(word) => {
-                            handleClick(word);
-                        }} word={getRandomEntry(words)} key={0} />
-                        <WordTile onClick={(word) => {
-                            handleClick(word);
-                        }} word={getRandomEntry(words)} key={1} />
-                        <WordTile onClick={(word) => {
-                            handleClick(word);
-                        }} word={getRandomEntry(words)} key={2} />
-                        <WordTile onClick={(word) => {
-                            handleClick(word);
-                        }} word={getRandomEntry(words)} key={3} />
-
-                    </div>
-
-                    <div className="container">
-                        <WordTile onClick={(word) => {
-                            handleClick(word);
-                        }} word={getRandomEntry(words)} key={4} />
-                        <WordTile onClick={(word) => {
-                            handleClick(word);
-                        }} word={getRandomEntry(words)} key={5} />
-                        <WordTile onClick={(word) => {
-                            handleClick(word);
-                        }} word={getRandomEntry(words)} key={6} />
-                        <WordTile onClick={(word) => {
-                            handleClick(word);
-                        }} word={getRandomEntry(words)} key={7} />
-                    </div>
-                    <div className="container">
-                        <WordTile onClick={(word) => {
-                            handleClick(word);
-                        }} word={getRandomEntry(words)} key={8} />
-                        <WordTile onClick={(word) => {
-                            handleClick(word);
-                        }} word={getRandomEntry(words)} key={9} />
-                        <WordTile onClick={(word) => {
-                            handleClick(word);
-                        }} word={getRandomEntry(words)} key={10} />
-                        <WordTile onClick={(word) => {
-                            handleClick(word);
-                        }} word={getRandomEntry(words)} key={11} />
-                    </div>
-                    <div className="container">
-                        <WordTile onClick={(word) => {
-                            handleClick(word);
-                        }} word={getRandomEntry(words)} key={12} />
-                        <WordTile onClick={(word) => {
-                            handleClick(word);
-                        }} word={getRandomEntry(words)} key={13} />
-                        <WordTile onClick={(word) => {
-                            handleClick(word);
-                        }} word={getRandomEntry(words)} key={14} />
-                        <WordTile onClick={(word) => {
-                            handleClick(word);
-                        }} word={getRandomEntry(words)} key={15} />
-                    </div>
+                    {[...Array(WIDTH)].map((_, rowIndex) => (
+                        <div className="container" key={rowIndex}>
+                            {[...Array(HEIGHT)].map((_, colIndex) => (
+                                <WordTile
+                                    onClick={(index) => {
+                                        handleClick(index);
+                                    }}
+                                    word={words[rowIndex * 4 + colIndex].word}
+                                    key={rowIndex * 4 + colIndex}
+                                    index={rowIndex * 4 + colIndex}
+                                    isClicked={choices.includes(rowIndex * 4 + colIndex)}
+                                />
+                            ))}
+                        </div>
+                    ))}
                 </div>
-            </div >
-            <button onClick={() => checkCorrect(choices)}>Submit</button>
+            </div>
+            <button onClick={() => checkCorrect(choices)}>Check</button>
         </>
     );
 }
--- src/components/WordTile.js
+++ src/components/WordTile.js
@@ -1,15 +1,12 @@
 import "./WordTile.css";
-import { useState } from "react";
-export default function WordTile({ word, onClick }) {
+export default function WordTile({ word, onClick, index,isClicked}) {
     //will eventually check group of 4 words
-    const [clicked, setClicked] = useState(false);
     const handleClick = () => {
-        onClick(word);
-        setClicked(!clicked);
+        onClick(index);
     }
     return (
         <div className="word-tile">
-            <button className= {"word-button "+ (clicked ? "clicked": "")} onClick={handleClick}>
+            <button className={"word-button " + ((isClicked) ? "clicked" : "")} onClick={handleClick}>
                 {word}
             </button>
         </div>
