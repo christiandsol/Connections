b95d6e88e8da99dfa10d6b8800b822ce3e4d9f34
Christian Solano <christiansolano@Christians-MacBook-Air-2.local>
2024-02-22 19:05:29 -0800

    Add color toggle functionality to buttons

--- src/components/WordTile.js
+++ src/components/WordTile.js
@@ -1,13 +1,15 @@
 import "./WordTile.css";
-
-export default function WordTile({ word, onClick}) {
+import { useState } from "react";
+export default function WordTile({ word, onClick }) {
     //will eventually check group of 4 words
+    const [clicked, setClicked] = useState(false);
     const handleClick = () => {
         onClick(word);
+        setClicked(!clicked);
     }
     return (
         <div className="word-tile">
-            <button className="word-button" onClick={handleClick}>
+            <button className= {"word-button "+ (clicked ? "clicked": "")} onClick={handleClick}>
                 {word}
             </button>
         </div>
